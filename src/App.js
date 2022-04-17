1183987e5b24c4f4f5c89dcc0a5cfd5af9b075d0
krasish <krasishontov@gmail.com>
2022-04-17 10:49:19 +0300

    Add ProuctAlbum component. Visualize skis.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import { CssBaseline } from "@mui/material";
 import React from "react";
-import "./App.css";
-import Home from "./pages/Home";
+import SkiAppBar from "./components/SkiAppBar.js";
+import { green, purple } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Home from "./pages/Home.js";
+
+const pages = [
+  { title: "Ski", subpages: ["Men Ski", "Women Ski", "Kids Ski"] },
+  {
+    title: "Ski Boots",
+    subpages: ["Men Ski Boots", "Women Ski Boots", "Kids Ski Boots"],
+  },
+];
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Home />
+      <ThemeProvider theme={theme}>
+        <SkiAppBar settings={settings} pages={pages} />
+        <CssBaseline />
+        <Home />
+      </ThemeProvider>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import ApiClient from "../service/api-client";
 import Box from "@mui/system/Box";
-import SkiAppBar from "../components/SkiAppBar.js";
-import Copyright from "../components/Copyright";
-
-const pages = [
-  { title: "Ski", subpages: ["Men Ski", "Women Ski", "Kids Ski"] },
-  {
-    title: "Ski Boots",
-    subpages: ["Men Ski Boots", "Women Ski Boots", "Kids Ski Boots"],
-  },
-];
-
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import ProductAlbum from "../components/ProductAlbum";
 
 function Home(props) {
   const [ski, setSki] = useState([]);
@@ -34,16 +26,48 @@ function Home(props) {
 
   return (
     <>
-      <SkiAppBar settings={settings} pages={pages} />
-      <Box>
-        Skis:
-        {ski.map((s) => (
-          <p key={s.id}>{JSON.stringify(s)}</p>
-        ))}
-        <br />
-        Errors: {errors.toString()}
-      </Box>
-      <Copyright />
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Album layout
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+          <ProductAlbum products={ski} />
+        </Box>
+      </main>
     </>
   );
 }
